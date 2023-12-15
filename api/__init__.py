2fa7a184df2bdacf65672f903c2af91995df3e01
erwin <erwin.stein@gmail.com>
2023-12-15 08:37:23 -0300

    added mongo service stop script

--- api/__init__.py
+++ api/__init__.py
@@ -6,10 +6,12 @@ from .routes.custumers import custumer
 def start_mongo_service():
     import subprocess
     from subprocess import call 
-    status = subprocess.check_output("systemctl show -p ActiveState --value mongodb.service",shell=True)
+    status = subprocess.check_output("systemctl show -p ActiveState --value mongodb.service",
+                                    shell=True,
+                                    universal_newlines=True).strip()   
     pwd='er'
     cmd='systemctl start mongodb.service'
-    if 'inactive' in str(status):
+    if status == 'inactive':
         call('echo {} | sudo -S {}'.format(pwd, cmd), shell=True)
         print('MONGO STARTED')
     else:
